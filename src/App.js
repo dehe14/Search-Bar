3519b7e8ef88563da982d33620e2285ffecad03d
Dehelean C <dehe1997@gmail.com>
2023-09-10 17:12:47 +0300

    Finished search bar functionality and basic styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
+import JSONDATA from "./MOCK_DATA.json";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  console.log(`${searchTerm}`);
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => {
+          setSearchTerm(event.target.value);
+        }}
+      ></input>
+      {JSONDATA.filter((val) => {
+        if (searchTerm === "") {
+          return val;
+        } else if (
+          val.first_name
+            .toLocaleLowerCase()
+            .includes(searchTerm.toLocaleLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
